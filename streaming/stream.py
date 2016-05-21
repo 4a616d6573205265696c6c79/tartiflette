3a0322059bae162c813f3777aa94ae469892d33b
4a616d6573205265696c6c79 <fun@fb.com>
2016-05-21 14:17:42 +0100

    Firehose filtering

--- streaming/stream.py
+++ streaming/stream.py
@@ -5,22 +5,27 @@ from ripe.atlas.cousteau import AtlasStream
 
 NETWORKS = {
     4: [
-        ipaddress.ip_network(net, strict=False) for net in
-        open('../Comcast-v4-Space').readlines()
+        ipaddress.ip_network(net.strip(), strict=False) for net in
+        open('../v4.txt').readlines()
     ],
     6: [
-        ipaddress.ip_network(net, strict=False) for net in
+        ipaddress.ip_network(net.strip(), strict=False) for net in
         open('../Comcast-v6-Space').readlines()
     ],
 }
 
-@asyncio.coroutine
 def filter_hop_rtt(*args):
     """Given a traceroute result, filter out the unnecessary data and
     hand off for analysis"""
-    print(args[0])
+    m_result = args[0]
+    if 'result' in m_result.keys() and m_result['result']:
+        for hop in m_result['result']:
+            if not 'result' in hop.keys():
+                continue
+            for address in hop['result']:
+                if 'from' in address.keys() and is_comcast_ip(address['from']):
+                    print(m_result)
 
-@asyncio.coroutine
 def is_comcast_ip(ip_address):
     """Returns true if this is a comcast IP address"""
     address = ipaddress.ip_address(ip_address)
@@ -29,16 +34,13 @@ def is_comcast_ip(ip_address):
             return True
     return False
 
-@asyncio.coroutine
 def stream_results(filters={}):
     atlas_stream = AtlasStream()
     atlas_stream.connect()
-    atlas_stream.bind_channel('result', on_result_response)
+    atlas_stream.bind_channel('result', filter_hop_rtt)
     stream_parameters = {"type": "traceroute"}
     atlas_stream.start_stream(stream_type="result", **stream_parameters)
-    atlas_stream.timeout()
+    atlas_stream.timeout(seconds=30)
 
 if __name__ == '__main__':
-    asyncio.get_event_loop()
-    loop.run_until_complete(pt.main())
-    loop.close()
+    stream_results()
