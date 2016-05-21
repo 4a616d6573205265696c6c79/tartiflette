ace997238c35f0bc4c868b89f0fa945a2b9cc57c
4a616d6573205265696c6c79 <fun@fb.com>
2016-05-21 15:27:04 +0100

    Add in support for multiple cores to process more data per second

--- streaming/stream.py
+++ streaming/stream.py
@@ -1,4 +1,17 @@
+__doc__ = """
+tartiflette
+
+Program to analyse real-time traceroute inormation for routing changes.
+
+Usage:
+    tartiflette --num_procs=<NUM>
+
+Options:
+    --num_procs=<NUM>   Number of worker processes to spin up to handle
+                        load. Uses one asyncio event loop per process.
+"""
 import asyncio
+import docopt
 import ipaddress
 import multiprocessing
 from ripe.atlas.cousteau import AtlasStream
@@ -71,12 +84,17 @@ def stream_results(filters={}):
     atlas_stream.connect()
     atlas_stream.bind_channel('result', on_result_recieved)
     stream_parameters = {"type": "traceroute"}
+    stream_parameters.update(filters)
     atlas_stream.start_stream(stream_type="result", **stream_parameters)
     atlas_stream.timeout()
 
 if __name__ == '__main__':
     """Start up one worker process to deal with handling checking traceroute
     results, and just use the main thread to read from atlas."""
-    proc = IPMatcher(WORK_QUEUE).start()
+    args = docopt.docopt(__doc__)
+    policy = asyncio.get_event_loop_policy()
+    policy.set_event_loop(policy.new_event_loop())
+    for i in range(int(args['--num_procs'])):
+        proc = IPMatcher(WORK_QUEUE).start()
     stream_results()
 
